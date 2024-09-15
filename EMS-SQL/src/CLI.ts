c9e4d86b69e39de2f4e6f8a94b1001c394aece93
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-09-15 03:40:22 -0600

    Testing Functions - need to add try catch blocks and fix add employee and update employee still

--- EMS-SQL/src/CLI.ts
+++ EMS-SQL/src/CLI.ts
@@ -1,5 +1,13 @@
 import inquirer from "inquirer";
-import { viewDepartments, addDepartment } from "./Queries.ts";
+import {
+  viewDepartments,
+  viewRoles,
+  viewEmployees,
+  addDepartment,
+  addRole,
+  addEmployee,
+  updateEmployeeRole,
+} from "./Queries.ts";
 
 async function mainMenu() {
   const answers = await inquirer.prompt([
@@ -24,14 +32,31 @@ async function mainMenu() {
     case "View All Departments":
       await viewDepartments();
       break;
+    case "View All Roles":
+      await viewRoles(); // Implement viewRoles function
+      break;
+    case "View All Employees":
+      await viewEmployees(); // Implement viewEmployees function
+      break;
     case "Add Department":
       await addDepartment();
       break;
-    // Add other cases for roles and employees
+    case "Add Role":
+      await addRole(); // Implement addRole function
+      break;
+    case "Add Employee":
+      await addEmployee(); // Implement addEmployee function
+      break;
+    case "Update Employee Role":
+      await updateEmployeeRole(); // Implement updateEmployeeRole function
+      break;
     case "Exit":
       console.log("Goodbye!");
       process.exit(0);
   }
+
+  // Recursively call mainMenu until the user chooses "Exit"
+  await mainMenu();
 }
 
 mainMenu();
--- EMS-SQL/src/Queries.ts
+++ EMS-SQL/src/Queries.ts
@@ -21,7 +21,10 @@ export const viewRoles = async () => {
 // employee ids, first names, last names, job titles, departments, salaries, and managers
 export const viewEmployees = async () => {
   const res = await query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager_id FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id"
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager_id 
+    FROM employee 
+    JOIN role ON employee.role_id = role.id 
+    JOIN department ON role.department = department.id`
   );
   console.table(res.rows);
 };
@@ -43,9 +46,101 @@ export const addDepartment = async () => {
 
 // TODO: Add a role
 // name, salary, and department for the role and that role is added
+export const addRole = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter the title of the role:",
+    },
+    {
+      type: "number",
+      name: "salary",
+      message: "Enter the salary for the role:",
+    },
+    {
+      type: "number",
+      name: "department",
+      message: "Enter the department id number for the role:",
+    },
+  ]);
+
+  await query(
+    "INSERT INTO role (title, salary, department) VALUES ($1, $2, $3)",
+    [answers.title, answers.salary, answers.department]
+  );
+  console.log(`Role ${answers.title} added successfully.`);
+};
 
 // TODO: Add an employee
 // first name, last name, role, and manager, and that employee is added to the database
+export const addEmployee = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "Enter the employee's first name:",
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "Enter the employee's last name:",
+    },
+    {
+      type: "number",
+      name: "role_id",
+      message: "Enter the role id for the employee:",
+    },
+    {
+      type: "number",
+      name: "manager_id",
+      message: "Enter the manager id for the employee:",
+    },
+  ]);
+
+  await query(
+    "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)",
+    [answers.first_name, answers.last_name, answers.role_id, answers.manager_id]
+  );
+  console.log(
+    `Employee ${answers.first_name} ${answers.last_name} added successfully.`
+  );
+};
 
 // TODO: Update an employee role
-// prompted to select an employee to update and their new role and this information is updated in the database 
+// prompted to select an employee to update and their new role and this information is updated in the database
+export const updateEmployeeRole = async () => {
+  const employees = await query("SELECT * FROM employee");
+  const roles = await query("SELECT * FROM role");
+
+  const employeeChoices = employees.rows.map((employee: any) => ({
+    name: `${employee.first_name} ${employee.last_name}`,
+    value: employee.id,
+  }));
+
+  const roleChoices = roles.rows.map((role: any) => ({
+    name: role.title,
+    value: role.id,
+  }));
+
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "employee_id",
+      message: "Select an employee to update:",
+      choices: employeeChoices,
+    },
+    {
+      type: "list",
+      name: "role_id",
+      message: "Select the employee's new role:",
+      choices: roleChoices,
+    },
+  ]);
+
+  await query("UPDATE employee SET role_id = $1 WHERE id = $2", [
+    answers.role_id,
+    answers.employee_id,
+  ]);
+  console.log("Employee role updated successfully.");
+};
